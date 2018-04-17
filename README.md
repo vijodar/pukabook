ac2076260e122fd0c091a71991554cf32734f9b6
cegrosa <vicentejodar97@gmail.com>
2018-04-18 00:08:14 +0200

    Test1

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# bookary
+# Bookary App
+App made with [Ionic Framework](https://ionicframework.com/) and created by [Vicente Jodar Martinez (AKA Cegrosa)](https://github.com/cegrosa) for student purposes
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -10,10 +10,10 @@ export class LoginPage implements OnHttpResponse {
 
   //regionConstructor
   constructor(public navCtrl: NavController) {
-    
+
   }
   //end region
-  
+
   //regionOnHttpResponse Interface
   onDataReceived(data) {
     throw new Error("Method not implemented.");
@@ -22,4 +22,34 @@ export class LoginPage implements OnHttpResponse {
     throw new Error("Method not implemented.");
   }
   //end region
+
+  public type = 'password';
+  public showPass = false;
+
+
+  showPassword() {
+    this.showPass = !this.showPass;
+
+    if (this.showPass) {
+      this.type = 'text';
+    } else {
+      this.type = 'password';
+    }
+  }
+
+  showPwd: boolean = false;
+  pwdType: string = "password";
+  pwdIcon: string = "md-eye-off";
+
+  showHidePassword() {
+    this.showPwd = !this.showPwd;
+
+    if (this.showPwd) {
+      this.pwdType = 'text';
+      this.pwdIcon = 'md-eye';
+    } else {
+      this.pwdType = 'password';
+      this.pwdIcon = 'md-eye-off';
+    }
+  }
 }
