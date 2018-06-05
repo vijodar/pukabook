948809788f3ab71a6131a7a0f5d3880be0d9a8ca
vicente <vicentejodar97@gmail.com>
2018-06-05 21:21:59 +0200

    Change POTO to Model dir

--- src/model/user.ts
+++ src/model/user.ts
@@ -1,21 +1,38 @@
 interface IUser {
+    id: number
+    user: string
     email: string
     pass: string
+    guser: boolean
     token: string
 }
 
 export class User {
 
     //region PUBLIC_VARIABLES
+    id: number
+    user: string
     email: string
     pass: string
+    guser: boolean
     token: string
     //endregion PUBLIC_VARIABLES
 
     //region CONST
-    constructor(iUser: IUser = { email: null, pass: null, token: null }) {
+    constructor(iUser: IUser = {
+        id: null,
+        user: null,
+        email: null,
+        pass: null,
+        guser: null,
+        token: null
+    }) {
+
+        this.id = iUser.id
+        this.user = iUser.user
         this.email = iUser.email
         this.pass = iUser.pass
+        this.guser = iUser.guser
         this.token = iUser.token
     }
     //endregion CONS T
