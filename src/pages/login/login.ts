2ea62cf429863c214d0c0e0e57e7d11f1dcdad4d
vicente <vicentejodar97@gmail.com>
2018-06-05 21:21:34 +0200

    SignIn SignUp
    Added animation transition between forms and fadein/out button that
    switch between forms

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -4,45 +4,71 @@ import { OnHttpResponse } from '../../interfaces/onHttpResponse'
 import { RestClientProvider } from '../../providers/rest-client/restClient'
 import { SigninComponent } from './../../components/signin/signin';
 import { SignupComponent } from '../../components/signup/signup';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html'
 })
-export class LoginPage implements OnHttpResponse {
+export class LoginPage {
+
+  //region CONSTANTS
+  private translateStrings = {
+    "LOGIN_SWITCH_SIGNIN": "LOGIN_SWITCH_SIGNIN",
+    "LOGIN_SWITCH_SIGNUP": "LOGIN_SWITCH_SIGNUP"
+  }
+  //endregion CONSTANTS
 
   //region PUBLIC_VARIABLES
   public pwdType: string = "password"
   public pwdIcon: string = "md-eye-off"
 
-  public signinStatus: String
-  public signupStatus: String
+  public signinStatus: string
+  public signupStatus: string
   //endregion PUBLIC_VARIABLES
 
+  //region PRIVATE_VARIABLES
+  private btnSwitchSignInString: string
+  private btnSwitchSignUpString: string
+  //endregion PRIVATE_VARIABLES
+
   //region CONST
-  constructor(public navCtrl: NavController, public rjs: RestClientProvider) {
+  constructor(public navCtrl: NavController,
+    public rjs: RestClientProvider,
+    private translate: TranslateService) {
+
     this.starter()
   }
   //endegion CONST
 
-  //region ONHTTPRESPONSE
-  onDataReceived(data) {
-    console.log(data)
-  }
-  onErrorReceivingData(message: any) {
-    throw new Error("Method not implemented.")
-  }
-  //endregion ONHTTPRESPONSE
-  
-  //region PUBLIC_METHODS
-  private starter(){
+  //region PRIVATE_METHODS
+  private starter() {
     this.signinStatus = "signinShow"
     this.signupStatus = "signupHide"
-  }
 
+    this.translate.get(this.translateStrings.LOGIN_SWITCH_SIGNIN)
+      .subscribe(value => {
+        this.btnSwitchSignInString = value
+      })
+    this.translate.get(this.translateStrings.LOGIN_SWITCH_SIGNUP)
+      .subscribe(value => {
+        this.btnSwitchSignUpString = value
+      })
+  }
+  //endregion PRIVATE_METHODS
+
+  //region PUBLIC_METHODS
   switchForm() {
+    var btnSwitchForm = document.getElementById("btnSwitchForm")
+    btnSwitchForm.classList.add("btnSwitchFadeOI")
+    btnSwitchForm.textContent = btnSwitchForm.textContent == this.btnSwitchSignUpString ? this.btnSwitchSignInString : this.btnSwitchSignUpString
+
     this.signinStatus = this.signinStatus == "signinHide" ? "signinShow" : "signinHide"
     this.signupStatus = this.signupStatus == "signupHide" ? "signupShow" : "signupHide"
+
+    setTimeout(() => {
+      btnSwitchForm.classList.remove("btnSwitchFadeOI")
+    }, 800);
   }
 
   //endregion PUBLIC_METHODS
