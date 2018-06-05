5d0cb69ef880e6bc703d3b68551d1d72a5dea653
vicente <vicentejodar97@gmail.com>
2018-06-05 21:20:24 +0200

    SignUp Errors
    Added dialog errors on insert email and passwords

--- src/components/signup/signup.ts
+++ src/components/signup/signup.ts
@@ -1,3 +1,4 @@
+import { ErrorDialogProvider } from './../../providers/error-dialog/error-dialog';
 import { Component } from '@angular/core';
 import { OnHttpResponse } from '../../interfaces/onHttpResponse';
 import { RestClientProvider } from '../../providers/rest-client/restClient';
@@ -9,13 +10,15 @@ import { RestClientProvider } from '../../providers/rest-client/restClient';
 export class SignupComponent implements OnHttpResponse {
 
   //region PUBLIC_VARIABLES
-  public singupEmail: any
-  public signupPass1: any
-  public signupPass2: any
+  public signupEmail: string
+  public signupPass1: string
+  public signupPass2: string
   //endregion PUBLIC_VARIABLES
 
   //region CONST
-  constructor(public rjs: RestClientProvider) {
+  constructor(public rjs: RestClientProvider,
+    public dialogError: ErrorDialogProvider) {
+
     this.starter()
   }
   //endregion CONST
@@ -24,30 +27,55 @@ export class SignupComponent implements OnHttpResponse {
   onDataReceived(data) {
     console.log(data)
   }
-  onErrorReceivingData(message: any) {
+  onErrorReceivingData(message: number) {
     throw new Error("Method not implemented.")
   }
   //endregion ONHTTPRESPONSE
 
   //region PUBLIC_METHODS
+  public signUpButton() {
+    if (this.checkEmailField() && this.checkPassField()) {
+      var userpass = btoa(this.signupEmail + ":" + this.signupPass1)
+      var auth = "Basic " + userpass
+      this.rjs.doRequest("POST", "login", auth + "", this)
+    }
+  }
+  //endregion PUBLIC_METHODS
+
+  //region PRIVATE_METHODS
   private starter() {
-    this.singupEmail = ""
+    this.signupEmail = ""
     this.signupPass1 = ""
     this.signupPass2 = ""
   }
 
-  eventHandler(keyCode) {
-    if (keyCode.keyCode == 13) {
-
+  private checkEmailField(): boolean {
+    if (this.signupEmail.length > 0) {
+      if (this.signupEmail.match(/^(\w|[_.])+\@[a-z]+\.[a-z]+/)) {
+        return true
+      } else {
+        this.dialogError.showErrorDialog(3)
+      }
+    } else {
+      this.dialogError.showErrorDialog(2)
     }
+    return false
   }
 
-  signUpButton() {
-    var userpass = btoa("pepe" + ":" + "pepe")
-    var auth = "Basic " + userpass
-    console.log(auth);
+  private checkPassField(): boolean {
+    if (this.signupPass1.length > 0 &&
+      this.signupPass2.length > 0) {
 
-    this.rjs.doRequest("GET", "login", auth + "", this)
+      if (this.signupPass1 == this.signupPass2) {
+        return true
+      } else {
+        this.dialogError.showErrorDialog(5)
+      }
+    }else{
+      this.dialogError.showErrorDialog(4)
+    }
+
+    return false
   }
-  //endregion PUBLIC_METHODS
+  //endregion PRIVATE_METHODS
 }
