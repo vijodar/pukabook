79c1080500352ee35ceed6036b31477829df2d17
vicente <vicentejodar97@gmail.com>
2018-06-05 21:17:27 +0200

    SideMenu
    Added sidemenu. Modify when rest of pages are made

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,12 @@ import { TranslateService } from '@ngx-translate/core';
 import { LoginPage } from './../pages/login/login';
 
 @Component({
-  templateUrl: 'app.html'
+    templateUrl: 'app.html'
 })
 export class MyApp {
     rootPage: any = LoginPage;
+
+    public pages: Array<{title: string, component: any}>;
 
     constructor(private translate: TranslateService, platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
         platform.ready().then(() => {
@@ -18,17 +20,20 @@ export class MyApp {
             splashScreen.hide();
         });
         this.initTranslate();
+
+        // this.pages = [
+        //     { title: 'Home', component: HomePage },
+        //     { title: 'List', component: ListPage }
+        // ];
     }
 
     initTranslate() {
-        // Set the default language for translation strings, and the current language.
         this.translate.setDefaultLang('en');
-
 
         if (this.translate.getBrowserLang() !== undefined) {
             this.translate.use(this.translate.getBrowserLang());
         } else {
-            this.translate.use('en'); // Set your language here
+            this.translate.use('en');
         }
 
     }
