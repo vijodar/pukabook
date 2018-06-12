787e6ed02f2ae6bb117a97bbb5f8d67d171f1648
vicente <vicentejodar97@gmail.com>
2018-06-12 17:20:00 +0200

    HomePage Finish
    - Now check if the user has read any book
    - If there are no books, show a message with button to ExplorePage
    - If there are books, show the last read book and similar books

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,3 +1,4 @@
+import { Book } from './../../model/book';
 import { UserDBProvider } from './../../providers/userdb/userdb';
 import { OnHttpResponse } from './../../interfaces/onHttpResponse';
 import { RestClientProvider } from './../../providers/rest-client/restClient';
@@ -14,24 +15,43 @@ export class HomePage implements OnHttpResponse {
 
   //region CONSTANTS
   private translateStrings = {
-    "HOME_TITLE": "HOME_TITLE"
+    "HOME_TITLE": "HOME_TITLE",
+    "HOME_NO_PENDINGS_MSG": "HOME_NO_PENDINGS_MSG",
+    "HOME_NO_PENDINGS_BTN": "HOME_NO_PENDINGS_BTN",
+    "HOME_PENDINGS_CONTINUE_READINGS": "HOME_PENDINGS_CONTINUE_READINGS",
+    "HOME_PENDINGS_SIMILAR": "HOME_PENDINGS_SIMILAR",
   }
   //endregion CONSTANTS
 
   //region PUBLIC_VARIABLES
   public title: string
+  public noPendingsMsg: string
+  public noPendingsBtn: string
+  public pendingsContinueBtn: string
+  public pendingsSimilar: string
 
-  public anyPending: boolean
+  public noPending: boolean
+
+  public lastBook: Book
+  public books: Book[]
+  public alines: number
   //endregion PUBLIC_VARIABLES
 
   //region ONHTTPRESPONSE
   onDataReceived(data) {
     var result = data.result
-    console.log(result);
-
     if (result.auth) {
       this.userdb.modifyUserToken(result.t)
-
+      if (result.lastbook && result.books) {
+        this.noPending = false
+        this.alines = result.lastbook.alines
+        delete result.lastbook.alines
+        this.lastBook = <Book>result.lastbook
+        console.log(this.lastBook);
+        this.books = result.books
+      } else {
+        this.noPending = true
+      }
     } else {
       this.userdb.getUser()
         .then(value => {
@@ -49,9 +69,9 @@ export class HomePage implements OnHttpResponse {
   //region CONSTRUCTOR
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
-    public translate: TranslateService,
-    public rjs: RestClientProvider,
-    public userdb: UserDBProvider
+    private translate: TranslateService,
+    private rjs: RestClientProvider,
+    private userdb: UserDBProvider
   ) {
 
     this.starter()
@@ -60,10 +80,23 @@ export class HomePage implements OnHttpResponse {
 
   //region PRIVATE_METHODS
   private starter() {
-    this.anyPending = true
+    this.noPending = true
+    this.lastBook = new Book()
 
     this.translate.get(this.translateStrings.HOME_TITLE)
       .subscribe(value => { this.title = value })
+
+    this.translate.get(this.translateStrings.HOME_NO_PENDINGS_BTN)
+      .subscribe(value => { this.noPendingsBtn = value })
+
+    this.translate.get(this.translateStrings.HOME_NO_PENDINGS_MSG)
+      .subscribe(value => { this.noPendingsMsg = value })
+
+    this.translate.get(this.translateStrings.HOME_PENDINGS_CONTINUE_READINGS)
+      .subscribe(value => { this.pendingsContinueBtn = value })
+
+    this.translate.get(this.translateStrings.HOME_PENDINGS_SIMILAR)
+      .subscribe(value => { this.pendingsSimilar = value })
 
     this.userdb.getUser()
       .then(value => {
