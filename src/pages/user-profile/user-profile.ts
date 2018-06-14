a924e5cc36352b462a46c1a38bc333f1fcbf88ff
vicente <vicentejodar97@gmail.com>
2018-06-14 05:27:51 +0200

    UserProfile execute userSettings Provider

--- src/pages/user-profile/user-profile.ts
+++ src/pages/user-profile/user-profile.ts
@@ -1,5 +1,7 @@
+import { TranslateService } from '@ngx-translate/core';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { UsersettingsProvider } from '../../providers/usersettings/usersettings';
 
 @IonicPage()
 @Component({
@@ -8,6 +10,31 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class UserProfilePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  //region CONSTANTS
+  private translateStrings = {
+    "USERPROFILE_TITLE": "USERPROFILE_TITLE",
   }
+  //endregion CONSTANTS
+
+  //region PUBLIC_VARIABLES
+  public title: string
+  //endregion PUBLIC_VARIABLES
+
+  //region CONSTRUCTOR
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public userSettings: UsersettingsProvider,
+    private translate: TranslateService) {
+
+    this.starter()
+  }
+  //endregion CONSTRUCTOR
+
+  //region PRIVATE_METHODS
+  private starter() {
+    this.translate.get(this.translateStrings.USERPROFILE_TITLE)
+    .subscribe(value => { this.title = value })
+  }
+  //endregion PRIVATE_METHODS
 }
